4b10561a0cd4622408ffe48c0d2b78afb6382744
Mattia Fasoli <fasoxy@gmail.com>
2023-06-06 14:47:04 +0200

    Prova commit

--- RemoteApiScanner/Controllers/EsecuzioniKiteRunnersController.cs
+++ RemoteApiScanner/Controllers/EsecuzioniKiteRunnersController.cs
@@ -64,6 +64,7 @@ namespace RemoteApiScanner.Controllers
             if (esecuzioniKiteRunner.link != null)
             {
                 esecuzioniKiteRunner.user = User.Identity.Name;
+                // Prova commit
                 ProcessStartInfo startInfo = new ProcessStartInfo() { FileName = "C:\\Windows\\system32\\cmd.exe", WorkingDirectory = @"C:\\Users\\leo1-\\Desktop\\kiterunner\\dist", Arguments = "/c kr scan host.txt -w routes-small.kite -o json > results.json" };
                 Process proc = new Process() { StartInfo = startInfo, };
                 proc.Start();
