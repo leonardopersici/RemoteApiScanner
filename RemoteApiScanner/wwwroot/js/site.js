ec5b2521ca0adb933d06f1ec80067a8a4eb69586
Mattia Fasoli <fasoxy@gmail.com>
2023-06-15 20:07:41 +0200

    Modifiche Front-End
    Spostata la funzione di centratura nel file site.js così Pietro è contento!

--- RemoteApiScanner/wwwroot/js/site.js
+++ RemoteApiScanner/wwwroot/js/site.js
@@ -2,3 +2,16 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+// Funzione per centrare il contenuto di un div tra header e footer
+function setCenterDivHeight() {
+    const centerDiv = document.querySelector('.container.d-flex');
+    const headerHeight = document.querySelector('header').offsetHeight;
+    const footerHeight = document.querySelector('footer').offsetHeight;
+    const windowHeight = window.innerHeight - 32; // In toeria il -32 non dovrebbe servire ma è necessario altrimenti non funziona correttamente la centratura.
+    const visibleHeight = windowHeight - headerHeight - footerHeight;
+    centerDiv.style.height = visibleHeight + 'px';
+}
+
+window.addEventListener('DOMContentLoaded', setCenterDivHeight);
+window.addEventListener('resize', setCenterDivHeight);
